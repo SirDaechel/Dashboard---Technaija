9067d89402c76c74264d60a2c740b860cfb3c7a2
SirDaechel <anayookpala26@gmail.com>
2024-07-31 23:04:32 +0100

    code refactor: delete auth pages

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <section className="flex items-center justify-center min-h-screen w-full bg-cover bg-fixed bg-center">
-      {children}
-    </section>
-  );
-};
-
-export default Layout;
